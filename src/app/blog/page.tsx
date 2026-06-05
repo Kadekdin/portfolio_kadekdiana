30f54485b9dc5957265dfce000054c26585f7c36
Kadekdin <dianaprasnti@gmail.com>
2026-06-05 09:21:36 +0800

    remove work routes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,7 @@
 import { Column, Heading, Meta, Schema } from "@once-ui-system/core";
 import { Mailchimp } from "@/components";
 import { Posts } from "@/components/blog/Posts";
-import { baseURL, blog, person } from "@/resources";
+import { baseURL, blog, person } from "@/resources"; //isi newsletter di sini
 
 export async function generateMetadata() {
   return Meta.generate({
